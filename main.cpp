29990d8368f5f1800abb1c586eedec10268f9601
Katerina-03 <90139279+Katerina-03@users.noreply.github.com>
2022-03-12 15:27:55 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,78 +1,40 @@
-#include <iostream>
-#include "fun.h"
-
+#include "Vector.h"
 
 using namespace std;
 
+teamplate <typename T>
+class Vector
+{
+private:
+    {
+        T*arr_; // указатель памяти
+        size_t size_ {}; // размерность
+        size_t capacity_{}; // max размер
+    }
+public:
+    {
+        Vector () //конструктор класса
+        {
+            arr_ = new T [1];
+            capacity_ = 1;
+        }
+
+        Vector (conts vector <T> v) // конструктор копирования
+        {
+            size_ = v.size;
+            arr = new int;
+            for ( int = i; i < size_; i++)
+            {
+                *arr = *v.arr;
+                cout << "Copy-Constructor" << endl;
+            }
+        }
+    }
+};
+
+
 int main()
 {
-    int a, inc, tmp;
-
-    cout << "values: \n";
-    cin >> a >> inc;
-    tmp = a;
-    inc_num_point(&a, &inc);
-    cout << "point + inc = " << a << endl;
-    inc_num_link(tmp, inc);
-    cout << "link + inc = " << tmp << endl;
-
-    cout << "number to reverse: \n";
-    cin >> a;
-    tmp = a;
-    reverse_num_point(&a);
-    cout << "point = " << a << endl;
-    reverse_num_link(tmp);
-    cout << "link = " << tmp << endl;
-
-    complexNumber complex_num, complex_tmp;
-    cout << "complex value: \n";
-    cin >> complex_num.x;
-    cin >> complex_num.ar;
-    cin >> complex_num.y;
-    complex_tmp = complex_num;
-    conjugate_num_point(&complex_num);
-    cout << "point num: " << complex_num.x << complex_num.ar << complex_num.y << 'i'<<endl;
-    conjugate_num_link(complex_tmp);
-    cout << "link num: " << complex_tmp.x << complex_tmp.ar << complex_tmp.y << 'i'<<endl;
-
-    int n, input, scal;
-    cout << "size of your matrix: \n";
-    cin >> n;
-    cout << "scalar to multiply: \n";
-    cin >> scal;
-    int **arr;
-    arr = (int **) malloc(n * sizeof(int*));
-    int tmp_arr[256][256];
-
-    cout << "enter matrix: \n";
-    for(int i=0; i<n; i++) {
-        arr[i] = (int *) malloc(n * sizeof(int));
-
-
-        for(int j=0; j<n; j++) {
-            cin >> input;
-            arr[i][j] = input;
-            tmp_arr[i][j] = input;
-        }
-    }
-    multiply_array_point(arr, &n, &scal);
-    cout << "multiply\n";
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    cout << endl << endl;
-    multiply_array_link(tmp_arr, n, scal);
-    cout << "multiply \n";
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            cout << tmp_arr[i][j] << " ";
-        }
-        cout << endl;
-    }
-
+    cout << "Hello world!" << endl;
     return 0;
 }
